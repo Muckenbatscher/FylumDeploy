b822f7c34d60e12d2a181251a2559ae4328ff5df
Muckenbatscher <christi.braeuner97@gmail.com>
2026-02-23 20:41:59 +0100

    added docker compose publish to apphost

--- FylumDeploy.AppHost/AppHost.cs
+++ FylumDeploy.AppHost/AppHost.cs
@@ -3,6 +3,7 @@ internal class Program
     private static void Main(string[] args)
     {
         var builder = DistributedApplication.CreateBuilder(args);
+        var compose = builder.AddDockerComposeEnvironment("compose");
 
         var githubWebhookSecret = builder.AddParameter("GithubWebhookSecret", secret: true);
         builder.AddProject<Projects.FylumDeploy_Webhook>("webhook")
