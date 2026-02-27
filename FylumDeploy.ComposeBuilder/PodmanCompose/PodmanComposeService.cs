001fabd54724b54e62e42b119f6c6efeee1d5d36
Muckenbatscher <christi.braeuner97@gmail.com>
2026-02-27 14:31:59 +0100

    added podman compose service and call in the deployment service

---
+++ FylumDeploy.ComposeBuilder/PodmanCompose/PodmanComposeService.cs
@@ -0,0 +1,28 @@
+﻿using FylumDeploy.ComposeBuilder.ProcessExecution;
+
+namespace FylumDeploy.ComposeBuilder.PodmanCompose;
+
+internal class PodmanComposeService : IPodmanComposeService
+{
+    private readonly IProcessExecutionService _processExecutionService;
+
+    public PodmanComposeService(IProcessExecutionService processExecutionService)
+    {
+        _processExecutionService = processExecutionService;
+    }
+
+    public async Task<bool> ComposeDownAsync(CancellationToken cancellationToken)
+    {
+        var command = $"podman compose down";
+        var processExecute = new ProcessExecute(command: command, workingDirectory: Directories.OutputDirectory);
+        var result = await _processExecutionService.ExecuteProcessAsync(processExecute, cancellationToken);
+        return result.WasSuccessful;
+    }
+    public async Task<bool> ComposeUpAsync(CancellationToken cancellationToken)
+    {
+        var command = $"podman compose up -d";
+        var processExecute = new ProcessExecute(command: command, workingDirectory: Directories.OutputDirectory);
+        var result = await _processExecutionService.ExecuteProcessAsync(processExecute, cancellationToken);
+        return result.WasSuccessful;
+    }
+}
