3c441327b4458217bce050eadb7fbd9d3c8e6700
Muckenbatscher <christi.braeuner97@gmail.com>
2026-02-28 09:25:03 +0100

    change output directory

--- FylumDeploy.ComposeBuilder/Directories.cs
+++ FylumDeploy.ComposeBuilder/Directories.cs
@@ -4,5 +4,5 @@ internal class Directories
 {
     public const string BuildDirectory = "/tmp/build";
     public const string IntermediatePublishDirectory = "/tmp/aspire-artifact";
-    public const string OutputDirectory = "/output";
+    public const string OutputDirectory = "/opt/fylum";
 }
