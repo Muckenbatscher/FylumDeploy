37bd8f2c3e2712abfdd3ac7c2521ac55a1ede586
Muckenbatscher <christi.braeuner97@gmail.com>
2026-02-28 11:50:38 +0100

    multi stage checkout of git commit to be compatible with git version without support for --revision flag

--- FylumDeploy.ComposeBuilder/RepoClone/RepoCloneService.cs
+++ FylumDeploy.ComposeBuilder/RepoClone/RepoCloneService.cs
@@ -2,22 +2,62 @@
 
 namespace FylumDeploy.ComposeBuilder.RepoClone;
 
-internal class RepoCloneService : IRepoCloneService
+internal class RepoCloneService(
+    IProcessExecutionService processExecutionService,
+    ILogger<RepoCloneService> logger)
+    : IRepoCloneService
 {
     private const string RepoCloneUrl = "https://github.com/Muckenbatscher/Fylum.git";
 
-    private readonly IProcessExecutionService _processExecutionService;
-
-    public RepoCloneService(IProcessExecutionService processExecutionService)
-    {
-        _processExecutionService = processExecutionService;
-    }
+    private readonly ILogger<RepoCloneService> _logger = logger;
+    private readonly IProcessExecutionService _processExecutionService = processExecutionService;
 
     public async Task<bool> CloneRepoAsync(string commitHash, CancellationToken cancellationToken)
     {
-        var command = $"git clone --depth 1 --revision {commitHash} {RepoCloneUrl} {Directories.BuildDirectory}";
-        var processExecute = new ProcessExecute(command);
-        var result = await _processExecutionService.ExecuteProcessAsync(processExecute, cancellationToken);
-        return result.WasSuccessful;
+        var initCommand = $"git init {Directories.BuildDirectory}";
+        var initProcess = new ProcessExecute(initCommand);
+        var initResult = await _processExecutionService.ExecuteProcessAsync(initProcess, cancellationToken);
+        if (!initResult.WasSuccessful)
+        {
+            _logger.LogError("Failed to initialize git repository at {directory}", Directories.BuildDirectory);
+            return false;
+        }
+        _logger.LogInformation("Initialized git repository at {directory}", Directories.BuildDirectory);
+
+        var remoteAddCommand = $"git remote add origin {RepoCloneUrl}";
+        var remoteAddProcess = new ProcessExecute(remoteAddCommand, Directories.BuildDirectory);
+        var remoteAddResul = await _processExecutionService.ExecuteProcessAsync(remoteAddProcess, cancellationToken);
+        if (!remoteAddResul.WasSuccessful)
+        {
+            _logger.LogError("Failed to add remote origin {repoUrl} to git repository at {directory}",
+                RepoCloneUrl, Directories.BuildDirectory);
+            return false;
+        }
+        _logger.LogInformation("Added remote origin {repoUrl} to git repository at {directory}",
+            RepoCloneUrl, Directories.BuildDirectory);
+
+        var fetchCommand = $"git fetch --depth 1 origin {commitHash}";
+        var fetchProcess = new ProcessExecute(fetchCommand, Directories.BuildDirectory);
+        var fetchReuslt = await _processExecutionService.ExecuteProcessAsync(fetchProcess, cancellationToken);
+        if (!fetchReuslt.WasSuccessful)
+        {
+            _logger.LogError("Failed to fetch commit {commitHash} from remote repository {repoUrl} in git repository at {directory}",
+                commitHash, RepoCloneUrl, Directories.BuildDirectory);
+            return false;
+        }
+        _logger.LogInformation("Fetched commit {commitHash} from remote repository {repoUrl} to set FETCH_HEAD",
+            commitHash, RepoCloneUrl);
+
+        var checkoutCommand = $"git checkout FETCH_HEAD";
+        var checkoutProcess = new ProcessExecute(checkoutCommand, Directories.BuildDirectory);
+        var checkoutResult = await _processExecutionService.ExecuteProcessAsync(checkoutProcess, cancellationToken);
+        if (!checkoutResult.WasSuccessful)
+        {
+            _logger.LogError("Failed to checkout FETCH_HEAD in git repository at {directory}", Directories.BuildDirectory);
+            return false;
+        }
+        _logger.LogInformation("Checked out FETCH_HEAD in git repository at {directory}", Directories.BuildDirectory);
+
+        return true;
     }
 }
